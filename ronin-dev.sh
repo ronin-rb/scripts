4ae66245808274c495f34274bd6c6e5dc95b9253
Postmodern <postmodern.mod3@gmail.com>
2022-08-01 02:14:42 -0700

    Fixed a typo in the dnf/yum dependency packages.

--- ronin-dev.sh
+++ ronin-dev.sh
@@ -274,7 +274,7 @@ function auto_install_bundler()
 function install_dependencies()
 {
 	case "$package_manager" in
-		dnf|yum)libraries=(readlin-devel sqlite-devel libxml2-devel libxslt-devel) ;;
+		dnf|yum)libraries=(readline-devel sqlite-devel libxml2-devel libxslt-devel) ;;
 		zypper)	libraries=(readline-devel sqlite3-devel libxml2-devel libxslt-devel) ;;
 		apt)	libraries=(libreadline-dev libsqlite3-dev libxml2-dev libxslt1-dev) ;;
 		*)	libraries=(readline sqlite libxml2 libxslt) ;;
