5d357f48dc37adcd92d0adc46df2560c120c9f0b
Postmodern <postmodern.mod3@gmail.com>
2023-02-26 05:02:09 -0800

    List the contents of the newly populated `~/src/ronin-rb/` directory.

--- ronin-dev.sh
+++ ronin-dev.sh
@@ -426,6 +426,12 @@ popd >/dev/null
 
 log "Successfully setup a development environment in ${ronin_src_dir}"
 
+echo
+for dir in "${ronin_src_dir}"/*; do
+	echo "	$dir"
+done
+echo
+
 if [[ ! "$ruby_version" == "3."* ]] && [[ "$package_manager" == "brew" ]]; then
 	warn "Ruby ${ruby_version} was installed via Homebrew."
 	warn "You will need to restart your shell or open a new terminal."
